03f430984a955ae504cdd13c13d3e048f808c943
Mani Bhushan <manibhushan.cs@gmail.com>
2017-06-27 07:43:23 -0700

    finger cmd problem statement

--- communications/finger_cmd.go
+++ communications/finger_cmd.go
@@ -9,6 +9,24 @@ import (
 	"errors"
 )
 
+/*
+Finger daemon
+
+Write a finger daemon that works with the finger(1) command.
+
+From the Debian package description:
+
+Fingerd is a simple daemon based on RFC 1196 that provides an interface to the "finger" program at most network sites.
+The program is supposed to return a friendly, human-oriented status report on either the system at the moment or a
+particular person in depth.
+
+Stick to the basics and only support a username argument. If the user has a .plan file show the contents of that file.
+So your program needs to be able to figure out:
+
+Does the user exist?
+If the user exists, show the contents of the .plan file.
+*/
+
 func main()  {
 	flag.Parse()
 	ln, err := net.Listen("tcp", ":79")
