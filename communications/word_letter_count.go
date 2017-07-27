a9f1bb3fbf048aba9e229e2a25602141d22dcb00
Mani Bhushan <manibhushan.cs@gmail.com>
2017-07-26 21:13:56 -0700

    word letter count

---
+++ communications/word_letter_count.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+Word and Letter Count
+
+Write a small program that reads text from standard input and performs the following actions:
+
+Count the number of characters (including spaces).
+Count the number of words.
+Count the numbers of lines
+In other words implement wc(1) (check you local manual page), however you only have to read from standard input.
+ */
