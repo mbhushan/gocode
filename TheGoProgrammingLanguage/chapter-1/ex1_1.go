b5237ffcf4b5f01d2c0a8d67f0e4a427bddbbb3a
Mani Bhushan <manibhushan.cs@gmail.com>
2017-07-27 07:42:29 -0700

    exercise 1.1

---
+++ TheGoProgrammingLanguage/chapter-1/ex1_1.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+/**
+Exercise 1.1: Modify the echo program to also print os.Args[0], the name of the command that invoked it.
+ */
+
+func main()  {
+	fmt.Println(strings.Join(os.Args[0:], " "))
+}
