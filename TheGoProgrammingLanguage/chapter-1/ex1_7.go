a0b23c1c754c05f2dc5b5ccf404be3b74b5cd6d1
Mani Bhushan <manibhushan.cs@gmail.com>
2017-07-29 14:31:50 -0700

    added problem statement

---
+++ TheGoProgrammingLanguage/chapter-1/ex1_7.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+Exercise 1.7: The function call io.Copy(dst, src) reads from src and writes to dst.
+Use it instead of ioutil.ReadAll to copy the response body to os.Stdout without
+requiring a buffer large enough to hold the entire stream. Be sure to check the error result of io.Copy.
+*/
+
+import (
+	"os"
+	"net/http"
+	"fmt"
+	"io"
+)
+
+func main()  {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		//b, err := ioutil.ReadAll(resp.Body)
+		b, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", b)
+
+	}
+}
+
