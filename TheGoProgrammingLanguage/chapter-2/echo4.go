74a27db28d8767b409b8a36e7c84aed309a3b81a
Mani Bhushan <manibhushan.cs@gmail.com>
2017-07-30 09:46:51 -0700

    Echo4 prints its command-line arguments.

---
+++ TheGoProgrammingLanguage/chapter-2/echo4.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+// Echo4 prints its command-line arguments.
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+
+	if !*n {
+		fmt.Println()
+	}
+}
