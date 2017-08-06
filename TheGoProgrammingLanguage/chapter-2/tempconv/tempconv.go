b9cd2afd2e8125bf66a5c22adf3e4fc90ff7f60c
Mani Bhushan <manibhushan.cs@gmail.com>
2017-08-06 05:54:27 -0700

    tempconv file

---
+++ TheGoProgrammingLanguage/chapter-2/tempconv/tempconv.go
@@ -0,0 +1,24 @@
+package tempconv
+
+// Package tempconv performs Celsius, Fahrenheit and Kelvin conversions.
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC Celsius = 0
+	BoilingC Celsius = 100
+	KELVIN = 273.15
+)
+
+func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
+
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+
+func (k Kelvin) String() string { return fmt.Sprintf("%g°F", k) }
+
+
